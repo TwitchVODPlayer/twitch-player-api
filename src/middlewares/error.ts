9cfa044deed9f99c4c7cb5da5f219a1def7e87a5
Dastan21 <loicd21@gmail.com>
2022-10-12 22:21:58 +0200

    fix fetchAPI function

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,10 +1,15 @@
 import { RouterContext } from '@koa/router'
 import { formatError } from '../utils/error.js'
 
-export default function error (ctx: RouterContext, error: Error | string = 'Error', status = 500, message = 'Unknown error', needRefresh = false): void {
+export default function error (ctx: RouterContext, error: Error | string = 'Error', status = 500, message = 'Unknown error'): void {
   if (error == null) error = 'Error'
   if (process.env.NODE_ENV === 'development') console.error(error)
-  if (error instanceof Error) error = error.message
+
+  let needRefresh
+  if (error instanceof Error) {
+    needRefresh = (error.cause as any).needRefresh
+    error = error.message
+  }
 
   ctx.response.status = status
   ctx.body = formatError({
